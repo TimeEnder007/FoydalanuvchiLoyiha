483869a6c14514a4b0411be66bdf34e280f8d982
Jumayev Nazarbek <zumaevnazarbek48@gmail.com>
2024-08-02 16:29:00 +0500

    first

---
+++ src/Controller/UserCreateAction.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Component\User\UserFactory;
+use App\Component\User\UserManager;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserCreateAction extends AbstractController
+{
+    public function __construct(private readonly UserFactory $userFactory, private readonly UserManager $userManager)
+    {
+    }
+
+    public function __invoke(User $data): User
+    {
+        $user = $this->userFactory->create(
+            $data->getEmail(),
+            $data->getPassword(),
+            $data->getFullname(),
+            $data->getSurname(),
+            $data->getAge()
+        );
+        $this->userManager->save($user, isNeadFlush:true);
+
+        return $user;
+    }
+
+}
